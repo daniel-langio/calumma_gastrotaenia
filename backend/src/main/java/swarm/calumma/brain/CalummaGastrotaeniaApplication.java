002045d5bb175d85f522e4718535e0de3fabd185
Daniel Langio <langio.tehiniavo@gmail.com>
2025-07-26 04:03:18 +0300

    chore: initialize backend

---
+++ backend/src/main/java/swarm/calumma/brain/CalummaGastrotaeniaApplication.java
@@ -0,0 +1,13 @@
+package swarm.calumma.brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalummaGastrotaeniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalummaGastrotaeniaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/swarm/calumma/brain/CalummaGastrotaeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package swarm.calumma.brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalummaGastrotaeniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
